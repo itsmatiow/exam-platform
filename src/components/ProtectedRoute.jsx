1266c799fcaa42fc35af7eb1d1f2612fe81edffc
Matiow <mahdikeshi110@gmail.com>
2025-12-22 20:54:41 +0330

    Add Eitaa requestContact button main16

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,17 +4,18 @@ import { useAuth } from "../context/AuthContext";
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  // ۱. اگر هنوز داریم چک میکنیم، صبر کن
+  // ۱. صبر برای لود شدن
   if (loading) {
     return <div className="p-10 text-center">درحال بررسی هویت...</div>;
   }
 
-  // ۲. اگر کاربر کلا وجود نداشت (مشکل عجیب) یا "کاربر جدید" بود
-  // باید بره صفحه لاگین (ثبت نام)
-  if (!user || user.isNew || !user.student_id) {
+  // ۲. شرط ورود ساده شد:
+  // فقط اگر "یوزر نیست" یا "شماره موبایل نداره" بفرستش بیرون.
+  // (شرط student_id و isNew رو حذف کردم چون دیگه فرم ثبت نام نداریم)
+  if (!user || !user.phone_number) {
     return <Navigate to="/login" replace />;
   }
 
-  // ۳. اگه همه چی اوکی بود، بفرما تو
+  // ۳. بفرما تو
   return children;
 }
